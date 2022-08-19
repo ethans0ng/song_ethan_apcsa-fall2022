f4c3f645d3562f1870939573cea795e3d0c2fdd9
ethans0ng <80185438+ethans0ng@users.noreply.github.com>
2022-08-19 10:54:49 -0700

    b

---
+++ Unit01/Unit1Students/Unit1-InstructionExamples/StarsRunner.java
@@ -0,0 +1,24 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes buyan = new StarsAndStripes();
+      buyan.printTwentyStars(); buyan.printTwentyDashes();
+      buyan.printTwentyStars(); buyan.printTwentyDashes();
+      buyan.printTwentyStars(); buyan.printTwentyDashes();
+      buyan.printTwentyStars(); buyan.printTwentyDashes();
+      buyan.printTwentyStars(); buyan.printTwentyDashes();
+      buyan.printABigBox(); 
+   }
+}
