11df273f6319aace7cd5672785569dd0579f64f5
ethans0ng <80185438+ethans0ng@users.noreply.github.com>
2022-08-19 11:27:32 -0700

    c

---
+++ Unit02/Unit2Students/Unit2-Assignments/Cube.java
@@ -0,0 +1,12 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name - Ethan Song
+//Date - 08/19/22
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		return side*side*6;
+	}
+}
---
+++ Unit02/Unit2Students/Unit2-Assignments/Trap.java
@@ -0,0 +1,14 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Ethan Song
+//Date - 08/19/22
+//Class - APCSA
+//Lab  - Line Runner
+
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (float)1/2*(base1+base2)*height;
+	}
+}
