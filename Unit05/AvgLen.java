062304440117aea28fcb3db390f0b861303bf371
ethans0ng <80185438+ethans0ng@users.noreply.github.com>
2022-08-24 11:41:52 -0700

    a

---
+++ Unit05/AvgLen.java
@@ -0,0 +1,13 @@
+package Unit05;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name - Ethan Song
+//Date - 08/24/22
+
+public class AvgLen
+{
+   public static double go( String a, String b )
+	{
+		return (float)(a.length() + b.length())/2;
+	}
+}
